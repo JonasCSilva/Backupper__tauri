73d33ccd42e040ce07923aa1652def257a4332ef
Jonas Silva <55038425+JonasCSilva@users.noreply.github.com>
2022-06-19 12:29:22 -0300

    Before Mantine

--- src/checkProjects.ts
+++ src/checkProjects.ts
@@ -2,25 +2,52 @@ import { Command } from "@tauri-apps/api/shell";
 import { homeDir, join } from "@tauri-apps/api/path";
 import { BaseDirectory, readDir } from "@tauri-apps/api/fs";
 
+/* ["/C", { "validator": "\\S+" }] */
+
 export async function checkProjects() {
-  console.log("Here!");
-
   const thisHomeDir = await homeDir();
-
-  const cwd = await join(thisHomeDir, "Projects");
-
-  /* ["/C", { "validator": "\\S+" }] */
-  const command = new Command("pwsh", ["/C", "ls"], {
-    cwd,
-  });
-
-  console.log(await (await command.execute()).stdout);
 
   const stackFolders = await readDir("Projects", {
     dir: BaseDirectory.Home,
   });
 
   for (const stackFolder of stackFolders) {
-    console.log(stackFolder);
+    console.log(stackFolder.name);
+
+    const projectFolders = await readDir(
+      await join("Projects", stackFolder.name as string),
+      {
+        dir: BaseDirectory.Home,
+      }
+    );
+
+    for (const projectFolder of projectFolders) {
+      console.log("-- " + projectFolder.name);
+
+      try {
+        await readDir(
+          await join(
+            "Projects",
+            stackFolder.name as string,
+            projectFolder.name as string
+          ),
+          {
+            dir: BaseDirectory.Home,
+          }
+        );
+
+        console.log("---- Directory");
+
+        // const cwd = await join(thisHomeDir, "Projects");
+
+        const result = await new Command("pwsh", ["/C", "git status"], {
+          cwd: projectFolder.path,
+        }).execute();
+
+        console.log(result.stdout);
+      } catch (error) {
+        console.log("---- File");
+      }
+    }
   }
 }
